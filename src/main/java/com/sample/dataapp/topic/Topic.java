d6de2e20c22d15af63696a64801553790fc4e1d7
JUSTIN <NDH00288@Nmcorp.Nissan.Biz>
2019-08-26 20:54:26 +0530

    Adding project files

---
+++ src/main/java/com/sample/dataapp/topic/Topic.java
@@ -0,0 +1,39 @@
+package com.sample.dataapp.topic;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Topic {
+
+	@Id
+	private String id;
+	private String name;
+	
+	public Topic() {
+		super();
+	}
+
+	public Topic(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/main/java/com/sample/dataapp/topic/TopicController.java
@@ -0,0 +1,27 @@
+package com.sample.dataapp.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+}
